fb307ccb5bcb2de00b9e587bbacdbcec7f10631c
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-01-02 21:44:29 -0300

    ex 5.6 e 5.7

--- main.py
+++ main.py
@@ -18,10 +18,25 @@ def imprimir_dez_primeiros_multiplos_de_tres():
         inicio += 1
 
 
+def tabuada():
+    fim = int(input('Digite o numero que deseja saber a tabuada: '))
+    inicio = int(input('Digite de onde devemos começar: '))
+    while inicio <= 10:
+        print(f"{inicio} x {fim} = {fim * inicio}")
+        inicio += 1
+
+
+def multiplica_com_soma():
+    primeiro_numero = 5
+    segundo_numero = 10
+
+
 def menu():
     print('escolha o exercicio a ser exibido')
     print('1 para imprimir impares')
     print('2 para imprimir os dez primeiros multiplos de 3')
+    print('3 para imprimir a tabuada de um numero')
+    print('4 para multiplicar utilizando a soma')
 
     escolha = int(input())
 
@@ -30,6 +45,10 @@ def menu():
             imprimir_impares()
         case 2:
             imprimir_dez_primeiros_multiplos_de_tres()
+        case 3:
+            tabuada()
+        case 4:
+            multiplica_com_soma()
 
 
 if __name__ == '__main__':
