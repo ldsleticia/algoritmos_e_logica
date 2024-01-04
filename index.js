62810959937740f5851e71f775179970ed0643d2
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-01-03 21:33:15 -0300

    exercicios em node

--- index.js
+++ index.js
@@ -23,5 +23,44 @@ function dezPrimeirosMultiplosDeTres() {
   }
 }
 
+function tabuada() {
+  const fim = 10;
+  let inicio = 1;
+
+  while (inicio <= fim) {
+    console.log(`${inicio}x${fim} = ${inicio * fim}`);
+    inicio++;
+  }
+}
+
+function multiplicaComSoma() {
+  let fim = 10;
+  let inicio = 5;
+  let resultado = 0;
+
+  while (fim > 0) {
+    resultado += inicio;
+    fim--;
+  }
+  console.log(resultado);
+}
+
+function divideComSubtracao() {
+  let dividendo = 10;
+  let divisor = 2;
+  let quociente = 0;
+  let resto = dividendo;
+
+  while (resto >= divisor) {
+    resto -= divisor;
+    quociente += 1;
+  }
+  console.log('sou o quociente', quociente);
+  console.log('sou o resto', resto);
+}
+
 // imprimirImpares()
-dezPrimeirosMultiplosDeTres();
+// dezPrimeirosMultiplosDeTres();
+// tabuada()
+// multiplicaComSoma()
+// divideComSubtracao()
